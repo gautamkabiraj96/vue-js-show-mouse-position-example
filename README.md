769d5636b2ac700d3e3b9b1b6e7e3c6f7dfea0e5
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-11-18 00:29:10 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vue-js-show-mouse-position-example
-Created with CodeSandbox
+# How to access cursor / mouse position in Vue.js | Example
+
+Learn how to access cursor / mouse position on moving the mouse (x and y coordinates) in Vue.js without any library with demo example. [Learn more](https://www.nightprogrammer.com/vue-js/how-to-access-cursor-mouse-position-in-vue-js-example/)
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h2>Mouse position:</h2>
+    <p>Mouse position X: {{ mousePosX }}</p>
+    <p>Mouse position Y: {{ mousePosY }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      mousePosX: 0,
+      mousePosY: 0,
+    };
+  },
+  mounted() {
+    document.addEventListener("mousemove", (event) => {
+      this.mousePosX = event.clientX;
+      this.mousePosY = event.clientY;
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #000000;
+  margin: 60px;
+}
+</style>
